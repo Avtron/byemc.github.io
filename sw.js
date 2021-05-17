ec1b3d79b86e2a5a11b52813eae1d4b07276daf6
Undercover Stamp <byemc@outlook.com>
2021-05-17 20:17:33 +0100

    Added FeedBurner

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20210517.1902';
+const cacheName = 'chirpy-20210517.1917';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
