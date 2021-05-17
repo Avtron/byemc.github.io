9516e1461c2308f7a041e334d0da4b22b83658e9
Undercover Stamp <byemc@outlook.com>
2021-05-17 21:07:29 +0100

    updated shit

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20210517.2003';
+const cacheName = 'chirpy-20210517.2007';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
