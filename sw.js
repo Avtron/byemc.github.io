fcda9f12fead745c062721b4cb7ae5cee032b009
Undercover Stamp <byemc@outlook.com>
2021-05-21 19:25:15 +0100

    Updated Time

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20210521.1824';
+const cacheName = 'chirpy-20210521.1825';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
